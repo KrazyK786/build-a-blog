af62f3d9576c9ab09c640fae951e05327de20d8c
Kristopher Johnson <krazyk786@gmail.com>
2017-02-11 16:25:30 -0600

    Added jinja2 library in app.yaml and main.py, created base.html template. Confirmed initial page success.

--- main.py
+++ main.py
@@ -15,11 +15,39 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    """Class to simplify other functions"""
+
+    def write(self, *a, **kw):
+        """Method to shorten write function"""
+
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        """Renders template"""
+
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        """renders template"""
+
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    """Handles incoming requests to '/'"""
+
     def get(self):
-        self.response.write('Hello world!')
+        """Handles incoming GET requests"""
+
+        self.render('base.html')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
